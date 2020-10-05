f45ad15a8744ccc36aaa8420bd51be128ea1f32f
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-10-05 16:02:43 -0400

    Implement insertionSort and selectionSort

---
+++ insertionSort.js
@@ -0,0 +1,12 @@
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    const current = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > current) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = current;
+  }
+  return array;
+}
